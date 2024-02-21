17db27f577ef3c369d2a20a369c8345f05b31847
mandeep-mahra <mandeepmehara1@gmail.com>
2024-02-21 13:59:46 +0530

    post data

--- api.js
+++ api.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const { db } = require('./firebase.js')  
+const bodyParser = require('body-parser')
 
 const app = express();
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     const hubChallenge = req.query['hub.challenge'];
     
     res.send(hubChallenge);
+});
+app.post('/', async (req, res) => {
+    const ref = db.collection('subscriptionData').doc('lDAJsezo5PQduLEW1yS1');
+    const time = (Date.now());
+    const result = await ref.set({
+        [time] : req.body
+    },{ merge: true }); 
+    res.send("200");
 });
 
 const port = process.env.PORT || 8000;
